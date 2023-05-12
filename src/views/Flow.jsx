fd183d4bf3dd96ad8b13a2d1c95af4d9218ba5b4
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-05-12 15:50:24 +0200

    update styling

--- src/views/Flow.jsx
+++ src/views/Flow.jsx
@@ -2,10 +2,13 @@ import CardStack from "../Components/CardStack";
 
 function Flow() {
 
-    return (  
+    return (
         <section className="flow">
-            <h1>Flow</h1>
-            <p>Lorem ipsum dolor sit amet consectetur. Volutpat quis velit feugiat quis. Eget faucibus lacus cum massa faucibus pharetra fames. Tempor ridiculus dui sed convallis. Lorem ipsum dolor sit amet consectetur. Volutpat quis velit feugiat quis. Eget faucibus lacus cum massa faucibus pharetra fames. Tempor ridiculus dui sed convallis.</p>
+            <section className="intro">
+                <h1>Flow</h1>
+                <p>Lorem ipsum dolor sit amet consectetur. Volutpat quis velit feugiat quis. Eget faucibus lacus cum massa faucibus pharetra fames.
+                    <br/>Tempor ridiculus dui sed convallis. Lorem ipsum dolor sit amet consectetur. Volutpat quis velit feugiat quis. Eget faucibus lacus cum massa faucibus pharetra fames. Tempor ridiculus dui sed convallis.</p>
+            </section>
             <section>
                 <CardStack />
             </section>
